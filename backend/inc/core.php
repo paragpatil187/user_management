99b550762b73a26a41d9010e34a71afb268b2b78
PARAG RAGHUNATH PATIL <paragpatil187@gmail.com>
2024-12-05 22:01:29 +0530

    changes

--- backend/inc/core.php
+++ backend/inc/core.php
@@ -7,6 +7,7 @@ class Core
     const DB_PASS = "";
     const DB_HOST = "localhost";
     const DB_USER = "root";
+    
     // const BASE_URL = 'http://user--management.rf.gdapi/backend/api/';
     // const DB_NAME = "if0_37851430_user_crud";
     // const DB_PASS = "aPmImU9LTj";
